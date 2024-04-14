dcf71f31e75579bae2e6d585fec2017ab149c598
Thomas <60945086+thomas-chernaik@users.noreply.github.com>
2024-04-14 22:10:37 +0100

    added patterns 3,4,5

--- Board.cs
+++ Board.cs
@@ -32,6 +32,7 @@ public class Board : MonoBehaviour
     public GameObject demon2;
     public GameObject demon3;
     public GameObject demon4;
+    public GameObject demon5;
 
     // Start is called before the first frame update
     void Start()
@@ -88,6 +89,9 @@ public class Board : MonoBehaviour
         //call each spells test function
         TestForSpell1();
         TestForSpell2();
+        TestForSpell3();
+        TestForSpell4();
+        TestForSpell5();
     }
     bool TestForItemsAtPositions(Vector2[] positions, int item)
     {
@@ -116,13 +120,13 @@ public class Board : MonoBehaviour
     }
     void TestForSpell1()
     {
-        bool spell1 = false;
+        bool spell = false;
         //test for spell 1
         for (int i=0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
             {
-                spell1 = true;
+                spell = true;
                 Vector2 tilePosition = new Vector2(i, j);
                 //if the tile is a glow berry
                 if (plantArray[i][j] == (int)SeedType.GlowBerry)
@@ -130,13 +134,13 @@ public class Board : MonoBehaviour
                     //check for the other glow berries, at (0,4), (4,0), (4,4) + (i,j)
                     Vector2[] glowBerryPositions = new Vector2[] { new Vector2(0,4) + tilePosition, new Vector2(4,0) + tilePosition, new Vector2(4,4) + tilePosition };
                     //check that there is a glow berry at each of the positions
-                    spell1 = spell1 && TestForItemsAtPositions(glowBerryPositions, (int)SeedType.GlowBerry);
+                    spell = spell && TestForItemsAtPositions(glowBerryPositions, (int)SeedType.GlowBerry);
                     //check for the daises at (1,1), (1,2), (1,3), (2,1), (2,3), (3,1), (3,2), (3,3) + (i,j)
                     Vector2[] daisyPositions = new Vector2[] { new Vector2(1,1) + tilePosition, new Vector2(1,2) + tilePosition, new Vector2(1,3) + tilePosition, new Vector2(2,1) + tilePosition, new Vector2(2,3) + tilePosition, new Vector2(3,1) + tilePosition, new Vector2(3,2) + tilePosition, new Vector2(3,3) + tilePosition };
                     //check that there is a daisy at each of the positions
-                    spell1 = spell1 && TestForItemsAtPositions(daisyPositions, (int)SeedType.Daisy);
+                    spell = spell && TestForItemsAtPositions(daisyPositions, (int)SeedType.Daisy);
                     //if the spell is true, then we cast the spell
-                    if (spell1)
+                    if (spell)
                     {
                         //drain the glow berries
                         DrainAtPositions(glowBerryPositions);
@@ -156,14 +160,14 @@ public class Board : MonoBehaviour
     }
     void TestForSpell2()
     {
-        bool spell2 = false;
+        bool spell = false;
         //test for spell 2
         //test for spell 1
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
             {
-                spell2 = true;
+                spell = true;
                 Vector2 tilePosition = new Vector2(i, j);
                 //if the tile is a glow berry
                 if (plantArray[i][j] == (int)SeedType.GlowBerry)
@@ -171,13 +175,13 @@ public class Board : MonoBehaviour
                     //check for the other glow berries, at (0,6), (6,0), (6,6) + (i,j)
                     Vector2[] glowBerryPositions = new Vector2[] { new Vector2(0, 6) + tilePosition, new Vector2(6, 0) + tilePosition, new Vector2(6, 6) + tilePosition };
                     //check that there is a glow berry at each of the positions
-                    spell2 = spell2 && TestForItemsAtPositions(glowBerryPositions, (int)SeedType.GlowBerry);
+                    spell = spell && TestForItemsAtPositions(glowBerryPositions, (int)SeedType.GlowBerry);
                     //check for the daisies at (1,1), (2,2), (3,3), (4,4),(5,5), (1,5), 2,4), (4,2), (5,1) + (i,j)
                     Vector2[] daisyPositions = new Vector2[] { new Vector2(1, 1) + tilePosition, new Vector2(2, 2) + tilePosition, new Vector2(3, 3) + tilePosition, new Vector2(4, 4) + tilePosition, new Vector2(5, 5) + tilePosition, new Vector2(1, 5) + tilePosition, new Vector2(2, 4) + tilePosition, new Vector2(4, 2) + tilePosition, new Vector2(5, 1) + tilePosition };
                     //check that there is a daisy at each of the positions
-                    spell2 = spell2 && TestForItemsAtPositions(daisyPositions, (int)SeedType.Daisy);
+                    spell = spell && TestForItemsAtPositions(daisyPositions, (int)SeedType.Daisy);
                     //if the spell is true, then we cast the spell
-                    if (spell2)
+                    if (spell)
                     {
                         //drain the glow berries
                         DrainAtPositions(glowBerryPositions);
@@ -185,6 +189,112 @@ public class Board : MonoBehaviour
                         DrainAtPositions(daisyPositions);
                         //instantiate demon2 at (3,3) + (i,j)
                         Instantiate(demon2, new Vector3(3 * tileSize + offset.x + i * tileSize, 3 * tileSize + offset.y + j * tileSize, -5), Quaternion.identity);
+
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    void TestForSpell3()
+    {
+        bool spell = false;
+        //test for spell 1
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                spell = true;
+                Vector2 tilePosition = new Vector2(i, j);
+                //if the tile is a daisy
+                if (plantArray[i][j] == (int)SeedType.Daisy)
+                {
+                    //check for the other daisies, at (0,1), (0,2), (0,3), (3,1), (3,2), (3,3), (2,3),(1,3), (1,0), (2,0), (3,0) + (i,j)
+                    Vector2[] daisyPositions = new Vector2[] { new Vector2(0, 1) + tilePosition, new Vector2(0, 2) + tilePosition, new Vector2(0, 3) + tilePosition, new Vector2(3, 1) + tilePosition, new Vector2(3, 2) + tilePosition, new Vector2(3, 3) + tilePosition, new Vector2(2, 3) + tilePosition, new Vector2(1, 3) + tilePosition, new Vector2(1, 0) + tilePosition, new Vector2(2, 0) + tilePosition, new Vector2(3, 0) + tilePosition };
+                    //check that there is a daisy at each of the positions
+                    spell = spell && TestForItemsAtPositions(daisyPositions, (int)SeedType.Daisy);
+                    //check for the roses at (1,1), (1,2), (2,1), (2,2) + (i,j)
+                    Vector2[] rosePositions = new Vector2[] { new Vector2(1, 1) + tilePosition, new Vector2(1, 2) + tilePosition, new Vector2(2, 1) + tilePosition, new Vector2(2, 2) + tilePosition };
+                    //check that there is a rose at each of the positions
+                    spell = spell && TestForItemsAtPositions(rosePositions, (int)SeedType.Rose);
+                    //if the spell is true, then we cast the spell
+                    if (spell)
+                    {
+                        //drain the daisies
+                        DrainAtPositions(daisyPositions);
+                        //drain the roses
+                        DrainAtPositions(rosePositions);
+                        //instantiate demon3 at (2,2) + (i,j)
+                        Instantiate(demon3, new Vector3(2 * tileSize + offset.x + i * tileSize, 2 * tileSize + offset.y + j * tileSize, -5), Quaternion.identity);
+
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    void TestForSpell4()
+    {
+        bool spell = false;
+        //test for spell 1
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                spell = true;
+                Vector2 tilePosition = new Vector2(i, j);
+                //if the tile is a daisy
+                if (plantArray[i][j] == (int)SeedType.Daisy)
+                {
+                    print("found daisy at " + i + ", " + j);
+                    //find daisies at (0,1), (2,0), (2,1), (1,4)
+                    Vector2[] daisyPositions = new Vector2[] { new Vector2(0, -1) + tilePosition, new Vector2(2, 0) + tilePosition, new Vector2(2, -1) + tilePosition, new Vector2(1, -3) + tilePosition };
+                    //check that there is a daisy at each of the positions
+                    spell = spell && TestForItemsAtPositions(daisyPositions, (int)SeedType.Daisy);
+                    //find daffodils at (-1,3), (0,4), (2,4), (3,3) + (i,j)
+                    Vector2[] daffodilPositions = new Vector2[] { new Vector2(-1, -2) + tilePosition, new Vector2(0, -3) + tilePosition, new Vector2(2, -3) + tilePosition, new Vector2(3, -2) + tilePosition };
+                    //check that there is a daffodil at each of the positions
+                    spell = spell && TestForItemsAtPositions(daffodilPositions, (int)SeedType.Daffodil);
+                    //if the spell is true, then we cast the spell
+                    if (spell)
+                    {
+                        //drain the daisies
+                        DrainAtPositions(daisyPositions);
+                        //drain the daffodils
+                        DrainAtPositions(daffodilPositions);
+                        //instantiate demon4 at (1,3) + (i,j)
+                        Instantiate(demon4, new Vector3(1 * tileSize + offset.x + i * tileSize, 3 * tileSize + offset.y + j * tileSize, -5), Quaternion.identity);
+
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    void TestForSpell5()
+    {
+
+        bool spell = false;
+        //test for spell 1
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                spell = true;
+                Vector2 tilePosition = new Vector2(i, j);
+                //if the tile is a rose
+                if (plantArray[i][j] == (int)SeedType.Rose)
+                {
+                    //check for roses at (2,0), (-1,-1), (1,-1), (3,-1), (-1,-2), (3,-2), (0,-3), (2,-3), (1,-4) + (i,j)
+                    Vector2[] rosePositions = new Vector2[] { new Vector2(2,0) + tilePosition, new Vector2(-1,-1) + tilePosition, new Vector2(1,-1) + tilePosition, new Vector2(3,-1) + tilePosition, new Vector2(-1,-2) + tilePosition, new Vector2(3,-2) + tilePosition, new Vector2(0,-3) + tilePosition, new Vector2(2,-3) + tilePosition, new Vector2(1,-4) + tilePosition };
+                    //check that there is a rose at each of the positions
+                    spell = spell && TestForItemsAtPositions(rosePositions, (int)SeedType.Rose);
+                    if (spell)
+                    {
+                        //drain the roses
+                        DrainAtPositions(rosePositions);
+                        //instantiate demon5 at (1,-3) + (i,j)
+                        Instantiate(demon5, new Vector3(1 * tileSize + offset.x + i * tileSize, -3 * tileSize + offset.y + j * tileSize, -5), Quaternion.identity);
 
                         return;
                     }
