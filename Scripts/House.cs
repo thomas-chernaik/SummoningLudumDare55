aeb8cd4bd4251f3c2b79e494eeb9af7810abed66
Thomas <60945086+thomas-chernaik@users.noreply.github.com>
2024-04-15 22:49:28 +0100

    world generation has fences now

--- Scripts/House.cs
+++ Scripts/House.cs
@@ -1,19 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class House : MonoBehaviour
 {
     public GameObject player;
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
+        if (other.gameObject == player)
+        {
+            Debug.Log("Player entered the house");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
