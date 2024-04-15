70a1c72fe2ff777e3dbf8a5c79ab81187bf42057
Thomas <60945086+thomas-chernaik@users.noreply.github.com>
2024-04-15 23:30:54 +0100

    Made House logic

--- Scripts/House.cs
+++ Scripts/House.cs
@@ -1,17 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class House : MonoBehaviour
 {
     public GameObject player;
+    public GameObject NewDayUI;
+    public Button closeButton;
+    public Button NewDayButton;
+    public GameManager gameManager;
+    
+    void Start()
+    {
+        NewDayUI.SetActive(false);
+        closeButton.onClick.AddListener(CloseNewDayUI);
+        NewDayButton.onClick.AddListener(TriggerNextDay);
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject == player)
         {
-            Debug.Log("Player entered the house");
-
+            NewDayUI.SetActive(true);
         }
+    }
+    void CloseNewDayUI()
+    {
+        NewDayUI.SetActive(false);
+    }
+    void TriggerNextDay()
+    {
+        NewDayUI.SetActive(false);
+        gameManager.TriggerNextDay();
     }
 }
